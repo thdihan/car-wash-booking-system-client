ccd41f38fe3cfb4eca11a5db97d93e1125eaeeac
thdihan <thdihan@outlook.com>
2024-09-07 19:48:52 +0600

    Scroll to top button added

--- src/components/layout/HomeLayout.tsx
+++ src/components/layout/HomeLayout.tsx
@@ -1,12 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "../ui/header/Header";
 import Footer from "../ui/footer/Footer";
+import ScrollToTop from "react-scroll-to-top";
+import { useEffect } from "react";
 
 const HomeLayout = () => {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
     return (
         <>
             <Header />
             <Outlet />
+            <ScrollToTop smooth />
             <Footer />
         </>
     );
